3bf8e598c95adcc8c753dc9526f865f916bcc606
imonweb <contact@imonweb.co.uk>
2022-03-06 22:17:47 +0000

    function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("200 is a great number")
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+    print("All good!")
+
+
+days_to_units(20)
+days_to_units(35)
+days_to_units(50)
+days_to_units(100)
